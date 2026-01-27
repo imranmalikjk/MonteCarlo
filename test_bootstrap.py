9a2ff817e79c185e7ccc3be8fce3bcfd2568b24e
imranmalikjk <99903163+imranjk@users.noreply.github.com>
2026-01-26 20:27:37 -0600

    test simulate8data

--- test_bootstrap.py
+++ test_bootstrap.py
@@ -19,26 +19,21 @@ def test_init_invalid_beta0():
     with pytest.raises(TypeError):
         BootstrapStandardError("1", 2, 1, 100)
 
-
 def test_init_invalid_beta1():
     with pytest.raises(TypeError):
         BootstrapStandardError(1, "2", 1, 100)
-
 
 def test_init_invalid_sigma_negative():
     with pytest.raises(ValueError):
         BootstrapStandardError(1, 2, -1, 100)
 
-
 def test_init_invalid_sigma_zero():
     with pytest.raises(ValueError):
         BootstrapStandardError(1, 2, 0, 100)
 
-
 def test_init_invalid_sigma_type():
     with pytest.raises(TypeError):
         BootstrapStandardError(1, 2, "1", 100)
-
 
 def test_init_invalid_N():
     with pytest.raises(ValueError):
@@ -51,19 +46,26 @@ def test_check_dataset_not_dataframe():
     with pytest.raises(TypeError):
         bs._check_dataset([1, 2, 3])
 
-
 def test_check_dataset_missing_columns():
-    bs = BootstrapStandardError(1, 2, 1, 10)
+    bs = BootstrapStandardError(1, 2, 1, 100)
     D = pd.DataFrame({"x": [0,1,2,3,4,5]})
     with pytest.raises(ValueError):
         bs._check_dataset(D)
 
-
 def test_check_dataset_valid():
-    bs = BootstrapStandardError(1, 2, 1, 10)
+    bs = BootstrapStandardError(1, 2, 1, 100)
     D = pd.DataFrame({
         "x": np.random.randn(10),
         "y": np.random.randn(10)
     })
-    # ne doit pas lever d'exception
     bs._check_dataset(D)
+
+#_simulate_data
+def test_simulate_data():
+    bs = BootstrapStandardError(1, 2, 1, 100)
+    data = bs._simulate_data()
+
+    assert isinstance(data, pd.DataFrame)
+    assert data.shape == (100, 2)
+    assert "x" in data.columns
+    assert "y" in data.columns
