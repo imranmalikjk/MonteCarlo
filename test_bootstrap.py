33938911ea37db58e7246875f964516b4a2bfae7
imranmalikjk <99903163+imranjk@users.noreply.github.com>
2026-01-27 18:55:40 -0600

    add testing

--- test_bootstrap.py
+++ test_bootstrap.py
@@ -47,7 +47,7 @@ def test_check_dataset_not_dataframe():
         bs._check_dataset([1, 2, 3])
 
 def test_check_dataset_missing_columns():
-    bs = BootstrapStandardError(1, 2, 1, 100)
+    bs = BootstrapStandardError(1, 2, 1, 1000)
     D = pd.DataFrame({"x": [0,1,2,3,4,5]})
     with pytest.raises(ValueError):
         bs._check_dataset(D)
@@ -67,5 +67,60 @@ def test_simulate_data():
 
     assert isinstance(data, pd.DataFrame)
     assert data.shape == (100, 2)
-    assert "x" in data.columns
-    assert "y" in data.columns
+    assert "x" in data.columns and "y" in data.columns
+
+def test_check_dataset_invalid(): 
+    bs = BootstrapStandardError(1, 2, 1, 10) 
+    with pytest.raises(TypeError): 
+        bs._check_dataset([0.2,0.55]) 
+    with pytest.raises(ValueError): 
+        bs._check_dataset(pd.DataFrame({"a": [1], "b": [2]}))
+
+
+def test_bootstrap_beta():
+    bs = BootstrapStandardError(1, 2, 1, 1000)
+    df = bs._simulate_data()
+
+    beta1_hat, se_boot, beta1_boot = bs._bootstrap_beta(df, B=500)
+
+    assert isinstance(beta1_hat, float)
+    assert isinstance(se_boot, float)
+    assert isinstance(beta1_boot, np.ndarray)
+    assert len(beta1_boot) == 500
+
+#_get_bootstrap
+
+def test_bootstrap_beta_parallel():
+    bs = BootstrapStandardError(1, 2, 1, 1000)
+    df = bs._simulate_data()
+
+    beta1_hat, se_boot, beta1_boot = bs._bootstrap_beta_parallel(df, B=500)
+
+    assert isinstance(beta1_hat, float)
+    assert isinstance(se_boot, float)
+    assert isinstance(beta1_boot, np.ndarray)
+    assert len(beta1_boot) == 500
+
+
+
+def test_get_bootstrap():
+    beta1_hat, se_boot, beta1_boot = get_bootstrap(
+        beta0=1, beta1=2, sigma=1, N=100, B=30, useParallelism=False
+    )
+
+    assert isinstance(beta1_hat, float)
+    assert isinstance(se_boot, float)
+    assert isinstance(beta1_boot, np.ndarray)
+    assert len(beta1_boot) == 30
+
+def test_beta1_boot():
+    np.random.seed(123)
+    tolerance = 0.05
+
+    bs = BootstrapStandardError(1, 2, 1, 1000)
+    D = bs._simulate_data()
+
+    beta1_hat, _, beta1_boot = bs._bootstrap_beta(D, B=(500))
+
+    assert abs(np.mean(beta1_boot) - beta1_hat) < tolerance
+
